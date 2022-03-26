5cc9e3407b557f4edd5e6ea6d416367e34c0f01f
paulo <12735004+paulotokimatu@users.noreply.github.com>
2022-03-26 15:19:56 -0600

    feat: add code for simple Svelte app and its bundle

---
+++ README.md
@@ -0,0 +1,1 @@
+Código relacionado ao vídeo "Svelte Após Compilação".
---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	let toggle = false;
+</script>
+
+<main>
+	<div>
+		Value: {toggle ? 'On' : 'Off'}
+	</div>
+	<button on:click={() => {toggle = !toggle}}>
+		Toggle!
+	</button>
+</main>
